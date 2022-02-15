4116c72020f0dc718b247a824c4778cf9ff1b9dd
alvilopez <34128078+alvilopez@users.noreply.github.com>
2022-02-15 20:08:50 +0100

    alopmarc - Creacion SpringBoot Maven Java 17 Building Jar

---
+++ just-travel-it/src/main/java/is2/justtravelit/JustTravelItApplication.java
@@ -0,0 +1,13 @@
+package is2.justtravelit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustTravelItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustTravelItApplication.class, args);
+	}
+
+}
---
+++ just-travel-it/src/test/java/is2/justtravelit/JustTravelItApplicationTests.java
@@ -0,0 +1,13 @@
+package is2.justtravelit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustTravelItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
