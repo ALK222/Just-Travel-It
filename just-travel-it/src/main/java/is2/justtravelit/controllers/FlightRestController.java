6f49080ab1010eb94d1ea3f3f3668053e5f05a8f
Alejandro Barrachina <alejandrobarrachina.02@gmail.com>
2022-04-22 11:49:33 +0200

    Mapeado `FlightRestController` cambiado

--- just-travel-it/src/main/java/is2/justtravelit/controllers/FlightRestController.java
+++ just-travel-it/src/main/java/is2/justtravelit/controllers/FlightRestController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,19 +28,19 @@ public class FlightRestController {
         return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
     }
 
-    @PostMapping("/flights/add")
+    @PostMapping("/flights")
     public ResponseEntity<FlightDTO> addFlight(@RequestBody FlightDTO request) {
         FlightDTO response = flightService.addFlight(request);
         return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
     }
 
-    @GetMapping("/flights/delete/{id}")
+    @DeleteMapping("/flights")
     public ResponseEntity<FlightDTO> deleteFlight(@PathVariable Long id) {
         FlightDTO response = flightService.deleteFlight(id);
         return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
     }
 
-    @PostMapping("/flights/update")
+    @PutMapping("/flights")
     public ResponseEntity<FlightDTO> updateFlight(@RequestBody FlightDTO request) {
         FlightDTO response = flightService.addFlight(request);
         return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
