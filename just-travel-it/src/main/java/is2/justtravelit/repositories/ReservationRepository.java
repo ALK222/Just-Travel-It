07b59dbc172fdc2665345f37729cb1161cda7815
Alejandro Barrachina <alejandrobarrachina.02@gmail.com>
2022-05-23 19:46:47 +0200

    Documentación en código para `ResrvationRepository`

--- just-travel-it/src/main/java/is2/justtravelit/repositories/ReservationRepository.java
+++ just-travel-it/src/main/java/is2/justtravelit/repositories/ReservationRepository.java
@@ -7,9 +7,22 @@ import org.springframework.stereotype.Repository;
 
 import is2.justtravelit.entities.Reservation;
 
+/**
+ * Repositorio de reservas para las operaciones en la base de datos
+ * 
+ * @see org.springframework.data.repository.CrudRepository
+ * @see org.springframework.stereotype.Repository
+ * @see Reservation
+ */
 @Repository
 public interface ReservationRepository extends CrudRepository<Reservation, Long> {
 
+    /**
+     * Devuelve un listado de reservas hechas por un usuario
+     * 
+     * @param id ID del usuario
+     * @return Listado de todas las reservas del usuario
+     */
     List<Reservation> findAllByUser(String id);
 
 }
