1691356fb27f0e031dca758c96b8822f45f64a1d
Alejandro Barrachina <alejandrobarrachina.02@gmail.com>
2022-05-23 19:38:08 +0200

    Documentación en código para `UserDTOToEntityMapper`

--- just-travel-it/src/main/java/is2/justtravelit/mappers/UserDTOToEntityMapper.java
+++ just-travel-it/src/main/java/is2/justtravelit/mappers/UserDTOToEntityMapper.java
@@ -3,10 +3,21 @@ package is2.justtravelit.mappers;
 import is2.justtravelit.dtos.UserDTO;
 import is2.justtravelit.entities.User;
 
+/**
+ * Mapper para usuarios de DTO a Entity
+ * 
+ * @see User
+ * @see UserDTO
+ */
 public class UserDTOToEntityMapper {
+
+    /**
+     * @param dto User DTO a transformar
+     * @return User
+     */
     public static User mapUserDTOToUser(UserDTO dto) {
         User entity = new User();
-        
+
         entity.setEmail(dto.getEmail());
         entity.setName(dto.getName());
         entity.setNif(dto.getNif());
