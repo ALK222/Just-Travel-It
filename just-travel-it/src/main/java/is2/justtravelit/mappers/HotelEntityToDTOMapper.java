527713405821e1d78cb1136b7967b55b8150a3ac
Alejandro Barrachina <alejandrobarrachina.02@gmail.com>
2022-05-23 19:28:08 +0200

    Documentación en código para `HotelEntityToDTOMapper`

--- just-travel-it/src/main/java/is2/justtravelit/mappers/HotelEntityToDTOMapper.java
+++ just-travel-it/src/main/java/is2/justtravelit/mappers/HotelEntityToDTOMapper.java
@@ -3,11 +3,21 @@ package is2.justtravelit.mappers;
 import is2.justtravelit.dtos.HotelDTO;
 import is2.justtravelit.entities.Hotel;
 
+/**
+ * Mapper para hoteles de Entity a DTO
+ * 
+ * @see Hotel
+ * @see HotelDTO
+ */
 public class HotelEntityToDTOMapper {
 
+    /**
+     * @param entity Hotel Entity a transformar
+     * @return HotelDTO
+     */
     public static HotelDTO mapHotelEntityToHotelDTO(Hotel entity) {
         HotelDTO dto = new HotelDTO();
-        
+
         dto.setCity(entity.getCity());
         dto.setStars(entity.getStars());
         dto.setName(entity.getName());
