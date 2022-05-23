af8ed93addeb6fb854c742a94c8a478d00bb5f7b
Alejandro Barrachina <alejandrobarrachina.02@gmail.com>
2022-05-23 19:44:23 +0200

    Documentación en código para `FlightRepository`

--- just-travel-it/src/main/java/is2/justtravelit/repositories/FlightRespository.java
+++ just-travel-it/src/main/java/is2/justtravelit/repositories/FlightRespository.java
@@ -7,9 +7,24 @@ import org.springframework.stereotype.Repository;
 
 import is2.justtravelit.entities.Flight;
 
+/**
+ * Repositorio de vuelos para las operaciones en la base de datos
+ * 
+ * @see org.springframework.data.repository.CrudRepository
+ * @see org.springframework.stereotype.Repository
+ * @see Flight
+ */
 @Repository
 public interface FlightRespository extends CrudRepository<Flight, Long> {
+    /**
+     * Devuelve un listado con todos los vuelos
+     */
     List<Flight> findAll();
 
+    /**
+     * Borra el vuelo dado un código de referencia
+     * 
+     * @param id ID del vuelo a borrar
+     */
     void deleteByID(Long id);
 }
