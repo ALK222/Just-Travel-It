3a3db3ed0b4755e3f3c18babf8176de164ff8ea9
Alejandro Barrachina <alejandrobarrachina.02@gmail.com>
2022-05-23 18:09:24 +0200

    Documentación en código para `Hotel`

--- just-travel-it/src/main/java/is2/justtravelit/entities/Hotel.java
+++ just-travel-it/src/main/java/is2/justtravelit/entities/Hotel.java
@@ -5,50 +5,75 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-
+/**
+ * Entity de hoteles
+ * 
+ * @see javax.persistence.Entity
+ */
 @Entity
 public class Hotel {
-    
+
     @Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
-	private String name;
-	private Integer stars;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private Integer stars;
     private String city;
 
-
-
+    /**
+     * @return ID del hotel en la base de datos
+     */
     public Long getId() {
         return this.id;
     }
 
+    /**
+     * @param id Nuevo ID del hotel en la based de datos
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     * @return Nombre del hotel
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * @param name Nuevo nombre del hotel
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * @return Estrellas del hotel
+     */
     public Integer getStars() {
         return this.stars;
     }
 
+    /**
+     * @param stars Nueva calificación del hotel
+     */
     public void setStars(Integer stars) {
         this.stars = stars;
     }
 
+    /**
+     * @return Ciudad del hotel
+     */
     public String getCity() {
         return this.city;
     }
 
+    /**
+     * @param city Nueva ciudad del hotel
+     */
     public void setCity(String city) {
         this.city = city;
     }
 
-    
 }
