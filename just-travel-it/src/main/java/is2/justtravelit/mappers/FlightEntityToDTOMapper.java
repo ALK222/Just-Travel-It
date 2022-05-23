f58577792155173b460f5be91b38271e932135e4
Alejandro Barrachina <alejandrobarrachina.02@gmail.com>
2022-05-23 19:20:56 +0200

    Documentación en código para `FlightEntityToDTOMapper`

--- just-travel-it/src/main/java/is2/justtravelit/mappers/FlightEntityToDTOMapper.java
+++ just-travel-it/src/main/java/is2/justtravelit/mappers/FlightEntityToDTOMapper.java
@@ -3,8 +3,18 @@ package is2.justtravelit.mappers;
 import is2.justtravelit.dtos.FlightDTO;
 import is2.justtravelit.entities.Flight;
 
+/**
+ * Mapper para vuelos de Entity a DTO
+ * 
+ * @see Flight
+ * @see FlightDTO
+ */
 public class FlightEntityToDTOMapper {
-    
+
+    /**
+     * @param entity Flight Entity a transformar
+     * @return FlightDTO
+     */
     public static FlightDTO mapFlightToFlightDTO(Flight entity) {
         FlightDTO dto = new FlightDTO();
 
@@ -12,10 +22,9 @@ public class FlightEntityToDTOMapper {
         dto.setAeropuertoSalida(AirportEntityToDTOMapper.mapAirportToAirportDTO(entity.getAeropuertoSalida()));
         dto.setCodigo(entity.getCodigo());
         dto.setFechaLlegada(entity.getFechaLlegada());
-        dto.setFechaSalida(entity.getFechaSalida());        
+        dto.setFechaSalida(entity.getFechaSalida());
 
         return dto;
     }
-   
 
 }
