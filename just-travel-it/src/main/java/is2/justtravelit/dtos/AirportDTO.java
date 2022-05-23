8aada8a3780ec936f3c877e7d878cb7eb01cd306
Alejandro Barrachina <alejandrobarrachina.02@gmail.com>
2022-05-23 17:24:45 +0200

    Documentación en código para `AirportDTO`

--- just-travel-it/src/main/java/is2/justtravelit/dtos/AirportDTO.java
+++ just-travel-it/src/main/java/is2/justtravelit/dtos/AirportDTO.java
@@ -1,40 +1,54 @@
 package is2.justtravelit.dtos;
 
-
-
+/**
+ * DTO de aeropuertos
+ */
 public class AirportDTO {
 
-    
     private String city;
     private String cod;
     private String name;
 
-
-    
-
+    /**
+     * @return Ciudad en la que se encuentra el aeropuerto
+     */
     public String getCity() {
         return this.city;
     }
 
+    /**
+     * @param city Nueva ciudad en la que se encuentra el aeropuerto
+     */
     public void setCity(String city) {
         this.city = city;
     }
 
+    /**
+     * @return Código del aeropuerto en la base de datos
+     */
     public String getCod() {
         return this.cod;
     }
 
+    /**
+     * @param cod Nuevo código del aeropuerto
+     */
     public void setCod(String cod) {
         this.cod = cod;
     }
 
+    /**
+     * @return Nombre del aeropuerto
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * @param name Nuevo nombre del aeropuerto
+     */
     public void setName(String name) {
         this.name = name;
     }
 
-    
 }
