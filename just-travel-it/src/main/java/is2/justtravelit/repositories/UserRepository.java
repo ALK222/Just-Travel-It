3e8620c0db1cc541cf870ea00559eb17683842e8
Alejandro Barrachina <alejandrobarrachina.02@gmail.com>
2022-05-23 19:49:24 +0200

    Documentación en código para `UserRepository`

--- just-travel-it/src/main/java/is2/justtravelit/repositories/UserRepository.java
+++ just-travel-it/src/main/java/is2/justtravelit/repositories/UserRepository.java
@@ -1,7 +1,5 @@
 package is2.justtravelit.repositories;
 
-
-
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
@@ -9,9 +7,26 @@ import org.springframework.stereotype.Repository;
 
 import is2.justtravelit.entities.User;
 
+/**
+ * Repositorio de usuarios para las operaciones en la base de datos
+ * 
+ * @see org.springframework.data.repository.CrudRepository
+ * @see org.springframework.stereotype.Repository
+ * @see User
+ */
 @Repository
-public interface UserRepository extends CrudRepository<User, Integer>{
+public interface UserRepository extends CrudRepository<User, Integer> {
+    /**
+     * Devuelve un usuario dado un nombre
+     * 
+     * @param name Nombre del usuario a buscar
+     * @return Entity del usuario con todos sus datos
+     */
     User findByName(String name);
+
+    /**
+     * Devuelve un listado con todos los usuarios de la aplicación
+     */
     List<User> findAll();
-       
+
 }
