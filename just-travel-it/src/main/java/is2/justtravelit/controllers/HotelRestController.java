8432bab6b3e88904b3ec957acec2d49ca9857450
Alejandro Barrachina <alejandrobarrachina.02@gmail.com>
2022-05-23 11:58:13 +0200

    Documentación en código para `HotelRestController`

--- just-travel-it/src/main/java/is2/justtravelit/controllers/HotelRestController.java
+++ just-travel-it/src/main/java/is2/justtravelit/controllers/HotelRestController.java
@@ -15,18 +15,41 @@ import org.springframework.web.bind.annotation.RestController;
 import is2.justtravelit.dtos.HotelDTO;
 import is2.justtravelit.services.HotelService;
 
+/**
+ * Controlador para todas las operaciones con hoteles
+ * 
+ * @see RestController
+ */
 @RestController
 public class HotelRestController {
 
     @Autowired
     private HotelService hotelService;
 
+    /**
+     * Muestra todos los hoteles disponibles en la base de datos
+     * 
+     * @return Listado de hoteles y un código HTTP dependiendo del resultado de
+     *         la operación
+     * @see ResponseEntity
+     * @see HotelDTO
+     */
     @GetMapping("/hotels")
     public ResponseEntity<List<HotelDTO>> getHotels() {
         List<HotelDTO> response = hotelService.getHotels();
         return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
     }
 
+    /**
+     * Añade un nuevo hotel a la base de datos
+     * 
+     * @param request Hotel a añadir
+     * @return Hotel creado y un código HTTP código dependiendo del resultado
+     *         de la operación
+     * @see ResponseEntity
+     * @see RequestBody
+     * @see HotelDTO
+     */
     @PostMapping("/hotels")
     public ResponseEntity<HotelDTO> addHotel(@RequestBody HotelDTO hotelDTO) {
         HotelDTO response = hotelService.addHotel(hotelDTO);
@@ -42,12 +65,32 @@ public class HotelRestController {
 
     }
 
+    /**
+     * Borrra un hotel de la base de datos
+     * 
+     * @param request Hotel a borrar
+     * @return Hotel borrado y un código HTTP dependiendo del resultado de la
+     *         operación
+     * @see ResponseEntity
+     * @see RequestBody
+     * @see HotelDTO
+     */
     @DeleteMapping("/hotels")
     public ResponseEntity<HotelDTO> deleteHotel(@RequestBody HotelDTO request) {
         HotelDTO response = hotelService.deleteHotel(request);
         return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
     }
 
+    /**
+     * Actualiza un hotel en la base de datos
+     * 
+     * @param request Hotel con los datos actualizados
+     * @return Hotel actualizado y un código HTTP dependiendo del resultado de
+     *         la operación
+     * @see ResponseEntity
+     * @see RequestBody
+     * @see HotelDTO
+     */
     @PutMapping("/hotels/update")
     public ResponseEntity<HotelDTO> updateHotel(@RequestBody HotelDTO request) {
         HotelDTO response = hotelService.addHotel(request);
