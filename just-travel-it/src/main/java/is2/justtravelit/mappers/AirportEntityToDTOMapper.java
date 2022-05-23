1a3b1ae034be4e790328da766a1fa4be02d4ea7d
Alejandro Barrachina <alejandrobarrachina.02@gmail.com>
2022-05-23 19:13:19 +0200

    Documentación en código para `AirportEntityToDTOMapper`

--- just-travel-it/src/main/java/is2/justtravelit/mappers/AirportEntityToDTOMapper.java
+++ just-travel-it/src/main/java/is2/justtravelit/mappers/AirportEntityToDTOMapper.java
@@ -3,10 +3,21 @@ package is2.justtravelit.mappers;
 import is2.justtravelit.dtos.AirportDTO;
 import is2.justtravelit.entities.Airport;
 
+/**
+ * Mapper para aeroupertos de Entity a DTO
+ * 
+ * @see Airport
+ * @see AirportDTO
+ */
 public class AirportEntityToDTOMapper {
+
+    /**
+     * @param entity Ariport Entity a transformar
+     * @return AirportDTO
+     */
     public static AirportDTO mapAirportToAirportDTO(Airport entity) {
         AirportDTO dto = new AirportDTO();
-        
+
         dto.setCity(entity.getCity());
         dto.setCod(entity.getCod());
         dto.setName(entity.getName());
