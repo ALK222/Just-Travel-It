865cee05c8438fa58c7909f90d9b6a459eeeb72d
Alejandro Barrachina <alejandrobarrachina.02@gmail.com>
2022-05-23 19:43:06 +0200

    Documentación en código para `AirportRepository`

--- just-travel-it/src/main/java/is2/justtravelit/repositories/AirportRepository.java
+++ just-travel-it/src/main/java/is2/justtravelit/repositories/AirportRepository.java
@@ -7,12 +7,33 @@ import org.springframework.stereotype.Repository;
 
 import is2.justtravelit.entities.Airport;
 
+/**
+ * Repositorio de aeropuertos para las operaciones en la base de datos
+ * 
+ * @see org.springframework.data.repository.CrudRepository
+ * @see org.springframework.stereotype.Repository
+ * @see Airport
+ */
 @Repository
 public interface AirportRepository extends CrudRepository<Airport, Long> {
+
+    /**
+     * Devuelve un listado con todos los aeropuertos
+     */
     List<Airport> findAll();
 
+    /**
+     * Borra el aeropuerto dado un código de referencia
+     * 
+     * @param cod Código del aeropuerto a borrar
+     */
     void deleteByCod(String cod);
 
+    /**
+     * Busca el aeropuerto dado un código de referencia
+     * 
+     * @param cod Código del aeropuerto a buscar
+     */
     Airport findByCod(String cod);
 
 }
