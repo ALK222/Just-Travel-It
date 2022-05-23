7dd2ed969b2153f1e141d04b760a1db86c80bb26
Alejandro Barrachina <alejandrobarrachina.02@gmail.com>
2022-05-23 19:36:38 +0200

    Documentación en código para `ReservationEntityToDTOMapper`

--- just-travel-it/src/main/java/is2/justtravelit/mappers/ReservationEntityToDTOMapper.java
+++ just-travel-it/src/main/java/is2/justtravelit/mappers/ReservationEntityToDTOMapper.java
@@ -3,11 +3,21 @@ package is2.justtravelit.mappers;
 import is2.justtravelit.dtos.ReservationDTO;
 import is2.justtravelit.entities.Reservation;
 
-public class ReservationEntityToDTOMapper
- {
+/**
+ * Mapper para reservas de Entity a DTO
+ * 
+ * @see Reservation
+ * @see ReservationDTO
+ */
+public class ReservationEntityToDTOMapper {
+
+    /**
+     * @param entity Airport Entity a transformar
+     * @return ReservationDTO
+     */
     public static ReservationDTO mapReservationEntityToReservationDTO(Reservation entity) {
         ReservationDTO dto = new ReservationDTO();
-        
+
         dto.setHotel(HotelEntityToDTOMapper.mapHotelEntityToHotelDTO(entity.getHotel()));
         dto.setGoFlight(FlightEntityToDTOMapper.mapFlightToFlightDTO(entity.getGoFlight()));
         dto.setCanceled(entity.isCanceled());
