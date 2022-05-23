fd596438831989b73dee35109aebd4577f8bce7b
Alejandro Barrachina <alejandrobarrachina.02@gmail.com>
2022-05-23 19:45:31 +0200

    Documentación en código para `HotelRepository`

--- just-travel-it/src/main/java/is2/justtravelit/repositories/HotelRespository.java
+++ just-travel-it/src/main/java/is2/justtravelit/repositories/HotelRespository.java
@@ -5,9 +5,21 @@ import org.springframework.stereotype.Repository;
 
 import is2.justtravelit.entities.Hotel;
 
+/**
+ * Repositorio de hotels para las operaciones en la base de datos
+ * 
+ * @see org.springframework.data.repository.CrudRepository
+ * @see org.springframework.stereotype.Repository
+ * @see Hotel
+ */
 @Repository
 public interface HotelRespository extends CrudRepository<Hotel, Long> {
 
+    /**
+     * Borra el hotel dado un código de referencia
+     * 
+     * @param id ID del hotel a borrar
+     */
     void deleteById(Long id);
 
 }
