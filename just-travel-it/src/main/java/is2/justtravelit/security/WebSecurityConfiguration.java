2d1c2a88c08352e2e11a8bd4c89d3aa319dbae01
alvilopez <34128078+alvilopez@users.noreply.github.com>
2022-05-26 17:27:15 +0200

    Dockerizado

--- just-travel-it/src/main/java/is2/justtravelit/security/WebSecurityConfiguration.java
+++ just-travel-it/src/main/java/is2/justtravelit/security/WebSecurityConfiguration.java
@@ -1,7 +1,6 @@
 package is2.justtravelit.security;
 
 import java.security.SecureRandom;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -34,31 +33,44 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
+		http.csrf().disable();
+
 		// UserRestController
 		
-		http.authorizeRequests().antMatchers(HttpMethod.GET,"/flights").permitAll();
-		http.authorizeRequests().antMatchers("/login").permitAll();
-		http.authorizeRequests().antMatchers("/signup").anonymous();
-		http.authorizeRequests().antMatchers("/new/password").hasAnyRole("USER", "ADMIN");
-		http.authorizeRequests().antMatchers("/users").hasAnyRole("ADMIN");
+		
+		http.authorizeRequests().antMatchers(HttpMethod.POST,"/login").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.POST,"/signup").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.POST,"/new/password").hasAnyRole("USER", "ADMIN");
+		http.authorizeRequests().antMatchers(HttpMethod.GET,"/users").hasAnyRole("ADMIN");
 
 		// ReservationRestController
 
-		http.authorizeRequests().antMatchers("/reservations").permitAll();
-		http.authorizeRequests().antMatchers("/reservations").permitAll();
-		http.authorizeRequests().antMatchers("/reservations").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.POST,"/reservations").hasAnyRole("USER");
+		http.authorizeRequests().antMatchers(HttpMethod.GET,"/reservations").hasAnyRole("USER");
+		http.authorizeRequests().antMatchers(HttpMethod.PUT,"/reservations").hasAnyRole("USER");
 		
 		// HotelRestController
 		
-		http.authorizeRequests().antMatchers("/hotels").permitAll();
+		
+		http.authorizeRequests().antMatchers(HttpMethod.POST,"/hotels").hasAnyRole("ADMIN");
+		http.authorizeRequests().antMatchers(HttpMethod.GET,"/hotels").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.PUT,"/hotels").hasAnyRole("ADMIN");
+		http.authorizeRequests().antMatchers(HttpMethod.DELETE,"/hotels").hasAnyRole("ADMIN");
+		
 		
 		//FlightRestController
 
-		http.authorizeRequests().antMatchers("/flights").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.POST,"/flights").hasAnyRole("ADMIN");
+		http.authorizeRequests().antMatchers(HttpMethod.GET,"/flights").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.PUT,"/flights").hasAnyRole("ADMIN");
+		http.authorizeRequests().antMatchers(HttpMethod.DELETE,"/flights").hasAnyRole("ADMIN");
 
 		//AirportRestController
 
-		http.authorizeRequests().antMatchers("/airports").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.POST,"/airports").hasAnyRole("ADMIN");
+		http.authorizeRequests().antMatchers(HttpMethod.GET,"/airports").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.PUT,"/airports").hasAnyRole("ADMIN");
+		http.authorizeRequests().antMatchers(HttpMethod.DELETE,"/airports").hasAnyRole("ADMIN");
 
 
 		
